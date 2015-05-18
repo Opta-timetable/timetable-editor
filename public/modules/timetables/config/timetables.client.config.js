c6e3dc6eb11b9be5a2df9fadc7931a6ee03528e0
Floyd Pink <imbleedingme@googlemail.com>
2015-05-18 17:57:43 -0400

    fix the menus -  - we do not need the half-baked Courses module being shown to the world for now  - the Timetable menu item could be just clickable than being a dropdown for now

--- public/modules/timetables/config/timetables.client.config.js
+++ public/modules/timetables/config/timetables.client.config.js
@@ -4,8 +4,6 @@
 angular.module('timetables').run(['Menus',
   function (Menus) {
     // Set top bar menu items
-    Menus.addMenuItem('topbar', 'Timetables', 'timetables', 'dropdown', '/timetables(/create)?');
-    Menus.addSubMenuItem('topbar', 'timetables', 'List Timetables', 'timetables');
-    Menus.addSubMenuItem('topbar', 'timetables', 'New Course', 'timetables/create');
+    Menus.addMenuItem('topbar', 'Timetables', 'timetables', 'item', '/timetables');
   }
 ]);
