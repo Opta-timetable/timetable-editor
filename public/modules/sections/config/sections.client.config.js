3a77ffeb16232aa91d0b5adfe52da4b32a95c018
Arun Kutty <kutty.a@gmail.com>
2016-03-25 01:01:54 +0530

    Iconography and layout adjustments for views until Plan

--- public/modules/sections/config/sections.client.config.js
+++ public/modules/sections/config/sections.client.config.js
@@ -6,8 +6,8 @@ angular.module('sections').run(['Menus',
 		// Set top bar menu items
     // I wanted to create a module called "class" but JS didn't allow that. Used "Sections" instead for the module.
     // However, let the end user see "Classes" itself as that would make more sense
-		Menus.addMenuItem('topbar', 'Classes', 'sections', 'dropdown', '/sections(/create)?', null, null, 2);
-		Menus.addSubMenuItem('topbar', 'sections', 'List Classes', 'sections');
-		Menus.addSubMenuItem('topbar', 'sections', 'New Class', 'sections/create');
+		Menus.addMenuItem('topbar', 'Sections', 'sections', 'dropdown', '/sections(/create)?', null, null, 2);
+		Menus.addSubMenuItem('topbar', 'sections', 'List Sections', 'sections');
+		Menus.addSubMenuItem('topbar', 'sections', 'New Section', 'sections/create');
 	}
 ]);
